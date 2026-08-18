788b83bcfcf401af897b87ff8992a897175cfb78
demon mami <toguro926@gmail.com>
2026-08-19 00:04:27 +0900

    Add fixed test chart manifest

---
+++ charts.js
@@ -0,0 +1,32 @@
+window.TEST_CHARTS = [
+  {
+    "id": "what-hurts-the-most",
+    "title": "What Hurts The Most (Topmodelz ReMix)",
+    "difficulty": "Gomen Yuuka [1.4x Rate]",
+    "entry": "what-hurts-the-most.osz"
+  },
+  {
+    "id": "over-the-fullereneshift",
+    "title": "Over the Fullereneshift",
+    "difficulty": "Eternity",
+    "entry": "over-the-fullereneshift.osz"
+  },
+  {
+    "id": "navi-98",
+    "title": "navi 98",
+    "difficulty": "genjuro's hell oni",
+    "entry": "navi-98.osz"
+  },
+  {
+    "id": "tool-assisted-speedcore",
+    "title": "Tool-Assisted Speedcore (TQBF Frame Advance RMX)",
+    "difficulty": "Frame Perfect",
+    "entry": "tool-assisted-speedcore.osz"
+  },
+  {
+    "id": "sunglow",
+    "title": "Sunglow (Sped Up Ver.)",
+    "difficulty": "Blazing Hope of the Dazzling Sun",
+    "entry": "sunglow.osz"
+  }
+];
